2873eece7aa43c3a6036663e2e58feec3ec365b0
lixiang <271896700@qq.com>
2025-08-11 16:42:19 +0800

    1

--- tiffanylamps/nuxt.config.ts
+++ tiffanylamps/nuxt.config.ts
@@ -3,9 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: [
-    '@unocss/nuxt'
+
   ],
   css: [
-    '@unocss/reset/tailwind.css'
+
   ]
 })
