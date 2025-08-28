f4ffadf3d0c196b11a516dbd04b881290e35d35c
lixiang <271896700@qq.com>
2025-08-28 18:25:33 +0800

    1

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -30,6 +30,7 @@ function refresh() {
           <h1 class="title" @click="refresh">豪蒂后台管理</h1>
           <el-menu mode="horizontal" :ellipsis="false" :default-active="$route.path" class="nav-menu" router>
             <el-menu-item index="/list">文章列表</el-menu-item>
+            <el-menu-item index="/lamps">产品列表</el-menu-item>
           </el-menu>
         </div>
       </el-header>
