b0678fd6789cd2aac34b7974aa18bc416fb35e5b
lixiang <271896700@qq.com>
2025-08-29 10:55:28 +0800

    1

--- admin/src/main.js
+++ admin/src/main.js
@@ -5,9 +5,11 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
-
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 const app = createApp(App)
-
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 app.use(ElementPlus, {
     locale: zhCn,
 })
